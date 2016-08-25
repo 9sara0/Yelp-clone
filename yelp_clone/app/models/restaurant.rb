c6396ed096623a8a50e2c46ade5f7e8c8f9781d3
Sara <sarah.alalaya@gmail.com>
2016-08-25 18:07:28 +0400

    Extending association

--- yelp_clone/app/models/restaurant.rb
+++ yelp_clone/app/models/restaurant.rb
@@ -1,10 +1,7 @@
 class Restaurant < ActiveRecord::Base
-  has_many :reviews do
-    def build_with_user(attributes = {}, user)
-      attributes[:user] ||=user
-      build(attributes)
-    end
-  end
+  has_many :reviews,
+        -> { extending WithUserAssociationExtension },
+        dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true
 
 
